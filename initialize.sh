6571845749dcafe671ef969ae8ae0e2f70f1fa90
Guxi11 <guxi11@isanbel.com>
2020-08-25 22:44:55 +0800

    feat: remove workflow if exsited

--- initialize.sh
+++ initialize.sh
@@ -2,6 +2,17 @@ workflows=(quick-face)
 workflows_dir=$PWD
 
 cd ~/Library/Application\ Support/Alfred/Alfred.alfredpreferences/workflows
+
+# remvoe workflow soft link if exsited
+for workflow in ${workflows[@]}
+do
+  if [ -e ${workflow} ]
+  then
+    rm ${workflow}
+  fi
+done
+
+# add workflow soft link
 for workflow in ${workflows[@]}
 do
   ln -s ${workflows_dir}/${workflow}
